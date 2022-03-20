78faa63392354ced2ed420cdb0d84760571fa336
MMOla92 <mazzieola@hotmail.com>
2022-03-20 19:44:30 +0100

    DS & A

--- dataReverse.js
+++ dataReverse.js
@@ -14,21 +14,22 @@
           [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
           */
 
-  function dataReverse(data) {
-    // Your code here
-    //I need to reverse the stream of data
-    //Once reversed should become 10101010  00001111  00000000  11111111
-    //Total number of bits will always be a multiple of 8
-    //create new array
-    //Loop through the array
-    //index [i] needs to +=8 
-    //reverse the date 
-    //use functions such as shift, reverse, push, pop if needed
-    
-    for (let i=0; i <= data.length-1; i++){
-    let revData = []
-    data[i] = data[data.length -8 -i]
-    data[data.length -8 -i] = revData[i]
-    }
-      return data.length
+function dataReverse(data) {
+  // Your code here
+  //I need to reverse the stream of data
+  //Once reversed should become 10101010 00001111 00000000 11111111
+  //Total number of bits will always be a multiple of 8
+  //create new array 
+  //Loop through the array
+  //reverse the data
+  //use functions such as shift, reverse, push, pop if needed
+
+  //create a variable where a new empty array will hold itself
+  let revData = []
+  //create a loop and create a new array from the array already given
+  while (data.length) {
+  // ... adds the new length of data
+  revData.push(...data.splice(data.length -8, data.length))
+  }
+    return revData
 }
